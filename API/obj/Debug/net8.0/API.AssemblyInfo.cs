25909e9aac4fe38db564697fa924178ea75bb8ca
EL-QASEMY MOHAMED <melqasemy2003@gmail.com>
2025-10-09 10:24:02 +0100

    end of section 5

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0f11f3079fba8e623ec9870d923f53523ffa729")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dee74a7d57de9714f30d717e2376c86b584d005d")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
